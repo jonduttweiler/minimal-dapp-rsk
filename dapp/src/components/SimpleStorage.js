615ded21ba65db69d911ae33512198911c665db5
jona <jonatanduttweiler@gmail.com>
2021-03-19 19:47:47 -0300

    Add metadata to txStatus

--- dapp/src/components/SimpleStorage.js
+++ dapp/src/components/SimpleStorage.js
@@ -1,10 +1,54 @@
 import Web3 from "web3";
 import React, { useState, useEffect, useRef } from "react";
-import { Button } from "./ui/Button";
-import { CircularProgress } from "@material-ui/core";
 
+import {
+  CircularProgress,
+  Grid,
+  Container,
+  Button,
+  withStyles,
+} from "@material-ui/core";
+
+import { TransactionStatus } from "./styled";
 import SimpleStorageArtifact from "../artifacts/SimpleStorage.json";
-import { Flex } from "./styled";
+
+const CustomButton = withStyles({
+  root: {
+    boxShadow: "none",
+    textTransform: "none",
+    fontSize: 16,
+    padding: "6px 12px",
+    border: "1px solid",
+    lineHeight: 1.5,
+    backgroundColor: "#0063cc",
+    borderColor: "#0063cc",
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+    "&:hover": {
+      backgroundColor: "#0069d9",
+      borderColor: "#0062cc",
+      boxShadow: "none",
+    },
+    "&:active": {
+      boxShadow: "none",
+      backgroundColor: "#0062cc",
+      borderColor: "#005cbf",
+    },
+    "&:focus": {
+      boxShadow: "0 0 0 0.2rem rgba(0,123,255,.5)",
+    },
+  },
+})(Button);
 
 const contractAbi = SimpleStorageArtifact.abi;
 
@@ -17,7 +61,7 @@ function SimpleStorage() {
   );
   const [value, setValue] = useState();
   const [loading, setLoading] = useState(false);
-  const [txStatus, setTxStatus] = useState();
+  const [txStatus, setTxStatus] = useState({status:"",metadata:""});
   const [inputValue, setInputValue] = useState("");
 
   useEffect(() => {
@@ -48,19 +92,20 @@ function SimpleStorage() {
       const promiEvent = contract.methods.set(value).send({ from: account });
       promiEvent
         .once("sending", (payload) => {
-          setTxStatus("sending");
+          setTxStatus({status:"sending"});
         })
         .once("sent", (payload) => {
-          setTxStatus("sent");
+          setTxStatus({status:"sent"});
         })
         .once("transactionHash", (hash) => {
-          setTxStatus("transactionHash");
+          setTxStatus({status:"transactionHash", metadata:`HASH: ${hash}`});
         })
         .once("receipt", (receipt) => {
-          setTxStatus("receipt");
+          setTxStatus({status:"receipt", metadata: JSON.stringify(receipt,null,3)});
+          
         })
         .on("confirmation", (confNumber, receipt, latestBlockHash) => {
-          setTxStatus("confirmed");
+          setTxStatus({status:"confirmed", metadata:`confNumber: ${confNumber},receipt:${ JSON.stringify(receipt,null,3)}, latestBlockHash:${latestBlockHash}`});
         })
         .on("error", (error) => {
           setTxStatus("error");
@@ -74,39 +119,75 @@ function SimpleStorage() {
   }
 
   return (
-    <Flex column>
-      <div>
-        <div>Contract address:</div>
-        <div>{address}</div>
-      </div>
+    <Container maxWidth="sm">
+      <Grid
+        container
+        direction="column"
+        justify="flex-start"
+        alignItems="center"
+        spacing={3}
+      >
+        <Grid item>Simple dapp</Grid>
+        <Grid item>
+          <div>Contract address:</div>
+          <div>{address}</div>
+        </Grid>
+        <Grid item>
+          <div>Value:</div>
+          <div>{value}</div>
+        </Grid>
+        <Grid container direction="column" alignItems="center" spacing={2}>
+          <Grid item>
+            <input
+              style={{ textAlign: "right" }}
+              type="text"
+              pattern="[0-9]*"
+              value={inputValue}
+              onChange={(e) => {
+                const re = /^[0-9\b]+$/;
+                if (e.target.value === "" || re.test(e.target.value)) {
+                  setInputValue(e.target.value);
+                }
+              }}
+            />
+          </Grid>
+          <Grid item>
+            <CustomButton
+              disabled={loading}
+              variant="contained"
+              color="primary"
+              onClick={setNewValue}
+            >
+              Set value
+            </CustomButton>
+          </Grid>
+          <Grid item>
+            <Grid container direction="row" justify="center">
+              {loading && (
+                <CircularProgress
+                  size={25}
+                  color="secondary"
+                ></CircularProgress>
+              )}
+              <TransactionStatus>{txStatus?.status}</TransactionStatus>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Grid container direction="column">
+              <Grid item style={{
+                maxWidth:"100%", 
+                overflow:"auto",
+                backgroundColor:"rgba(200,200,200,0.8)",
+                
 
-      <Flex column center style={{ border: "2px solid tomato" }}>
-        <div style={{textAlign:"center"}}>value:</div>
-        <div style={{textAlign:"center"}}>{value}</div>
-      </Flex>
-      
-      <Flex row>
-        <input
-          style={{ textAlign: "right" }}
-          type="text"
-          pattern="[0-9]*"
-          value={inputValue}
-          onChange={(e) => {
-            const re = /^[0-9\b]+$/;
-            if (e.target.value === "" || re.test(e.target.value)) {
-              setInputValue(e.target.value);
-            }
-          }}
-        />
-        <Button onClick={setNewValue}>Set value</Button>
-        <Flex m={"0px 15px"}>
-          {loading && (
-            <CircularProgress size={25} color="secondary"></CircularProgress>
-          )}
-          {txStatus}
-        </Flex>
-      </Flex>
-    </Flex>
+                }}>
+                {txStatus?.metadata}
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
--- dapp/src/components/styled.js
+++ dapp/src/components/styled.js
@@ -1,14 +1,27 @@
 import styled from 'styled-components';
 
+export const TransactionStatus = styled.div`
+  padding:5px 25px;
+  font-family: "Montserrat";
+  font-size:20px;
+  font-weight:bold;
+`
 
-export const Flex = styled.div`
-  display:flex;
-  ${props => props.column && "flex-direction: column;"}
-  ${props => props.row && "flex-direction: row;"}
+export const Div = styled.div`
+  ${props => props.absolute && `position: absolute;`}
+  ${props => props.relative && `position: relative;`}
   ${props => props.m && `margin: ${props.m};`}
+  ${props => props.p && `padding: ${props.padding};`}
+  
+`
+
+export const Flex = styled(Div)`
+  display:flex;
   ${props => props.j && `justify-content: ${props.j};`}
   ${props => props.justify && `justify-content: ${props.justify};`}
   ${props => props.a && `align-content: ${props.a};`}
   ${props => props.align && `align-content: ${props.align};`}
   ${props => props.center && `align-content: center; justify-content:center;`}
+  ${props => props.column && "flex-direction: column;"}
+  ${props => props.row && "flex-direction: row;"}
 `
